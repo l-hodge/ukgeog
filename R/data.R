65fbb2561af803f983f1f9c0f07b384b4ab3076a
Louis Hodge <louishodge@hotmail.co.uk>
2020-05-12 21:53:14 +0100

    ADD: Add 2019 LEA to UTLA lookup

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' LEA Lookup.
+#'
+#' LEAs have different codes to ONS UTLAs.
+#'
+#' @format A data frame with 3 variables:
+#' \code{UTLA19CD},
+#' \code{LEA19CD} and
+#' \code{UTLA19NM}
+"lea2019lookup"
